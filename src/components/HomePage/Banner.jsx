ad34067845b8e259327a4551916e98d3538a4260
arjunn2313 <arjunnks123@gmail.com>
2024-09-08 21:54:44 +0530

    button color adjusted

--- src/components/HomePage/Banner.jsx
+++ src/components/HomePage/Banner.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import bannerImage from '../../assets/banner.webp';
+import React from "react";
+import { Link } from "react-router-dom";
+import bannerImage from "../../assets/banner.webp";
 
 const Banner = () => {
   return (
     <section
       className="relative h-screen bg-cover bg-center"
-      style={{ backgroundImage: "url('https://img.freepik.com/free-photo/view-car-running-high-speed_23-2150635443.jpg?t=st=1725811641~exp=1725815241~hmac=c6270b4ab1948ce7303b1b413d669661be9f7a71057c3c7924cffdd15e179c58&w=1380')" }}
+      style={{
+        backgroundImage:
+          "url('https://img.freepik.com/free-photo/view-car-running-high-speed_23-2150635443.jpg?t=st=1725811641~exp=1725815241~hmac=c6270b4ab1948ce7303b1b413d669661be9f7a71057c3c7924cffdd15e179c58&w=1380')",
+      }}
     >
       <div className="bg-black bg-opacity-60 w-full h-full flex flex-col justify-center items-center text-white text-center p-6 md:p-12">
         {/* Animation for Banner Text */}
@@ -14,19 +17,20 @@ const Banner = () => {
           Welcome to Our Emission Test Center
         </h1>
         <p className="text-lg md:text-xl mb-6 animate-fadeIn font-light font-poppins">
-          Providing accurate and reliable emission tests for all types of vehicles.
+          Providing accurate and reliable emission tests for all types of
+          vehicles.
         </p>
         {/* Button Styles and Hover Effects */}
         <div className="space-x-4">
           <Link
             to="/booking"
-            className="px-4 py-2 md:px-6 md:py-3 bg-blue-700 text-white rounded-lg shadow-lg transform hover:bg-blue-800 transition-all duration-300 ease-in-out"
+            className="px-4 py-2 md:px-6 md:py-3 border text-white  shadow-lg transform hover:bg-white hover:text-black transition-all duration-300 ease-in-out"
           >
             Book a Test
           </Link>
           <Link
             to="/services"
-            className="px-4 py-2 md:px-6 md:py-3 border-2 border-blue-300 text-blue-300 rounded-lg hover:bg-blue-300 hover:text-white transition-all duration-300 transform hover:scale-105 shadow-lg"
+            className="px-4 py-2 md:px-6 md:py-3 border-2   bg-white text-black   hover:bg-transparent hover:text-white transition-all duration-300 transform hover:scale-105 shadow-lg"
           >
             Learn More
           </Link>
@@ -37,4 +41,3 @@ const Banner = () => {
 };
 
 export default Banner;
-
