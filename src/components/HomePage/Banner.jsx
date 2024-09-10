c60ead968ba6282c93244b41dc237996f2d8b8a9
arjunn2313 <arjunnks123@gmail.com>
2024-09-10 18:53:12 +0530

    review link

--- src/components/HomePage/Banner.jsx
+++ src/components/HomePage/Banner.jsx
@@ -32,24 +32,25 @@ const HeroSection = () => {
           <div className="border-2 border-red-400 relative w-52 h-52  md:w-96 md:h-96 bg-gradient-to-r from-purple-700   via-indigo-500   to-violet-600 shadow-lg rounded-xl p-4 flex flex-col justify-center items-center text-white text-center font-bold transform rotate-45">
             <div className="-rotate-45 flex flex-col text-center items-center">
               <FaCar className="text-yellow-300 sm:text-3xl mb-2 animate-bounce" />{" "}
-              <span className="text-xs md:text-xl uppercase tracking-wider text-yellow-400">
+              <span className="text-md md:text-xl uppercase tracking-wider text-yellow-400">
                 Emission Test
               </span>
-              <span className="text-xl md:text-6xl mt-2 font-extrabold text-yellow-300">
+               {/* <FaCheckCircle className="text-green-500 text-2xl my-1" /> */}
+               <span className="text-sm sm:text-lg mt-2">
+                <span className="text-gray-200">Was:</span>{" "}
+                <span className="line-through text-white ">$25.00</span>
+              </span>
+              <span className="text-3xl sm:text-6xl">
+                <span className="text-gray-200">Now:</span>{" "}
+                <span className="text-green-400 font-bold">$13.99</span>
+              </span>
+              <span className="text-xl md:text-5xl mt-2 font-extrabold text-yellow-300">
                 44%
               </span>
               <span className="text-xl md:text-4xl font-bold text-yellow-300">
                 OFF
               </span>
-              <FaCheckCircle className="text-green-500 text-2xl my-1" />
-              <span className="text-sm sm:text-lg mt-2">
-                <span className="text-gray-200">Was:</span>{" "}
-                <span className="line-through text-gray-300">$25.00</span>
-              </span>
-              <span className="text-sm sm:text-2xl">
-                <span className="text-gray-200">Now:</span>{" "}
-                <span className="text-green-400 font-bold">$13.99</span>
-              </span>
+             
               <FaExclamationTriangle className="hidden sm:flex text-red-400 sm:text-xl mt-2 animate-pulse" />
               <span className="hidden sm:flex text-xs sm:text-lg uppercase text-red-400 tracking-wide animate-pulse font-bold">
                 Limited Offer!
